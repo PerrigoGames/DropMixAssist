f2c7ac8d9dcdfe8a68b73587c56c40ab29ae088e
Corey Perrigo <corey@perrigogames.com>
2020-12-12 12:02:01 -0800

    Initial Commit

---
+++ DropmixAssist/DropmixAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropmixAssistApp.swift
+//  DropmixAssist
+//
+//  Created by Corey Perrigo on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct DropmixAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
